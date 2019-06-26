2617aae12872438fc38c0ccb359634a0f5538681
ThxNo <feixie@thoughtworks.com>
2019-06-26 08:51:34 +0800

    rename another

--- src/main/java/com/practice/chance/Chance.java
+++ src/main/java/com/practice/chance/Chance.java
@@ -11,11 +11,11 @@ public class Chance {
         return new Chance(1 - this.fraction);
     }
 
-    public Chance and(Chance other) {
-        return new Chance(this.fraction * other.fraction);
+    public Chance and(Chance another) {
+        return new Chance(this.fraction * another.fraction);
     }
 
-    public Chance or(Chance other) {
-        return this.not().and(other.not()).not();
+    public Chance or(Chance another) {
+        return this.not().and(another.not()).not();
     }
 }
