5221572dea0ab0e2498806d136824edd3fa5187f
ThxNo <feixie@thoughtworks.com>
2019-06-27 10:51:21 +0800

    add checkstyle and jacoco plugins

--- src/main/java/com/practice/chance/Chance.java
+++ src/main/java/com/practice/chance/Chance.java
@@ -1,21 +1,21 @@
 package com.practice.chance;
 
 public class Chance {
-    private Double fraction;
+	private Double fraction;
 
-    public Chance(Double fraction) {
-        this.fraction = fraction;
-    }
+	public Chance(Double fraction) {
+		this.fraction = fraction;
+	}
 
-    public Chance not() {
-        return new Chance(1 - this.fraction);
-    }
+	public Chance not() {
+		return new Chance(1 - this.fraction);
+	}
 
-    public Chance and(Chance another) {
-        return new Chance(this.fraction * another.fraction);
-    }
+	public Chance and(Chance another) {
+		return new Chance(this.fraction * another.fraction);
+	}
 
-    public Chance or(Chance another) {
-        return this.not().and(another.not()).not();
-    }
+	public Chance or(Chance another) {
+		return this.not().and(another.not()).not();
+	}
 }
