81a472f6c5a307de4d9cdf8d68186dfea324db60
ThxNo <feixie@thoughtworks.com>
2019-06-27 10:56:59 +0800

    remove magic number

--- src/test/java/com/practice/chance/ChanceTest.java
+++ src/test/java/com/practice/chance/ChanceTest.java
@@ -18,27 +18,29 @@ public class ChanceTest {
     @Test
     public void should_chance_not_right() {
         //given
+        double expectedFraction = 0.667d;
         //when
         //then
         assertThat(diceChance.not()).isEqualToComparingFieldByField(
-                new Chance(0.667d));
+                new Chance(expectedFraction));
     }
 
     @Test
     public void should_chance_and_right() {
         //given
-        //when
-        //then
+        double expectedFraction = 0.333d * 0.5d;
+        //when        //then
         assertThat(diceChance.and(coinChance)).isEqualToComparingFieldByField(
-                        new Chance(0.333d * 0.5d));
+                        new Chance(expectedFraction));
     }
 
     @Test
     public void should_chance_or_right() {
         //given
+        double expectedFraction = 1d - (1 - 0.333d) * (1 - 0.5d);
         //when
         //then
         assertThat(diceChance.or(coinChance)).isEqualToComparingFieldByField(
-                new Chance(1d - (1 - 0.333d) * (1 - 0.5d)));
+                new Chance(expectedFraction));
     }
 }
